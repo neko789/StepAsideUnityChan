65613af836188596f8413885b166a67160520d33
neko7890-THINK\neko7890 <nice.hermori@gmail.com>
2018-03-06 13:40:53 +0900

    家でヤッタやつ

--- Assets/UnityChan/Models/UnityChanController.cs
+++ Assets/UnityChan/Models/UnityChanController.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UnityChanController : MonoBehaviour {
 
@@ -14,6 +15,13 @@ public class UnityChanController : MonoBehaviour {
 
     private float movableRange = 3.4f;
 
+    private float upForce = 500.0f;
+
+    private float coefficient = 0.95f;
+
+    private bool isEnd = false;
+
+    private GameObject stateText;
 
 	// Use this for initialization
 	void Start () {
@@ -22,10 +30,20 @@ public class UnityChanController : MonoBehaviour {
         this.myAnimator.SetFloat("Speed", 1);
 
         this.myRigidbody = GetComponent<Rigidbody>();
+
+        this.stateText = GameObject.Find("GameResultText");
 	}
 	
 	// Update is called once per frame
 	void Update () {
+
+        if (this.isEnd)
+        {
+            this.forwardForce *= this.coefficient;
+            this.turnForce *= this.coefficient;
+            this.upForce *= this.coefficient;
+            this.myAnimator.speed *= this.coefficient;
+        }
 
         this.myRigidbody.AddForce(this.transform.forward * this.forwardForce);
 
@@ -37,5 +55,31 @@ public class UnityChanController : MonoBehaviour {
         {
             this.myRigidbody.AddForce(this.turnForce, 0, 0);
         }
+        if (this.myAnimator.GetCurrentAnimatorStateInfo(0).IsName("Jump"))
+        {
+            this.myAnimator.SetBool("Jump", false);
         }
-	}
+        if (Input.GetKeyDown(KeyCode.Space) &&this.transform.position.y < 0.5f){
+            this.myAnimator.SetBool("Jump", true);
+            this.myRigidbody.AddForce(this.transform.up * this.upForce);
+        }
+        }
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "CarTag"|| other.gameObject.tag == "TrafficConeTag")
+        {
+            this.isEnd = true;
+            this.stateText.GetComponent<Text>().text = "GAME OVER";
+        }
+        if(other.gameObject.tag == "GoalTag")
+        {
+            this.isEnd = true;
+
+        }
+        if(other.gameObject.tag == "CoinTag")
+        {
+            GetComponent<ParticleSystem>().Play();
+            Destroy(other.gameObject);
+        }
+    }
+}
